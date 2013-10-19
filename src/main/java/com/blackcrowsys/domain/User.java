dcbad7654157c81f8a721389c49e63bb0002d29a
rs146 <rs146.london@gmail.com>
2013-10-19 21:05:32 +0100

    Add User bean to the project.

---
+++ src/main/java/com/blackcrowsys/domain/User.java
@@ -0,0 +1,31 @@
+package com.blackcrowsys.domain;
+
+public class User {
+	private Integer id;
+	private String login;
+	private String password;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
