5a34a9cb8dfe6c8b60b60b22bee700b07b1e49c6
rs146 <rs146.london@gmail.com>
2013-10-30 21:19:49 +0000

    Added User Role class to the Project and changed the LocalSessionFactoryBean to obtain the sessionFactory instance.

--- src/main/java/com/blackcrowsys/domain/User.java
+++ src/main/java/com/blackcrowsys/domain/User.java
@@ -1,18 +1,29 @@
 package com.blackcrowsys.domain;
 
+import java.io.Serializable;
+import static javax.persistence.GenerationType.IDENTITY;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+/**
+ * 
+ * @author rahulsingh
+ * Entity class for users in the database. Database - BIND_DB
+ */
 @Entity
 @Table(name = "users")
-public class User {
+public class User implements Serializable {
+	
+	private static final long serialVersionUID = 1794281823969368244L;
 	private Integer id;
 	private String login;
 	private String password;
 
 	@Id
+	@GeneratedValue(strategy = IDENTITY)
 	@Column(name = "id")
 	public Integer getId() {
 		return id;
